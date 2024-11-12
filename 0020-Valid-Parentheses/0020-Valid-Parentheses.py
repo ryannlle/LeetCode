8be471b39c9f0260f3ff056f7772528a95ace0aa
ryan <ryankle71@gmail.com>
2024-11-11 23:56:47 -0800

    Runtime 0ms | Beats 100% Memory 11.84 MB | Beats 16.08%

--- 0020-Valid-Parentheses/0020-Valid-Parentheses.py
+++ 0020-Valid-Parentheses/0020-Valid-Parentheses.py
@@ -6,15 +6,11 @@ class Solution(object):
         """
 
         opcl = dict(('()', '[]', '{}'))
-
         stack = []
-
         for idx in s:
 
             if idx in '([{':
                 stack.append(idx)
-
             elif len(stack) == 0 or idx != opcl[stack.pop()]:
                 return False
-
         return len(stack) == 0
